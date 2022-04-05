c249d5325d10caf1144cdb25bef59be7f9374764
Nitheeshshetty <83216828+Nitheeshshetty@users.noreply.github.com>
2022-04-05 19:36:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomDiceImages="dice"+randomNumber1+".png";
+
+var randomImageSource="images/"+randomDiceImages;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImages2="dice"+randomNumber2+".png";
+
+var randomImageSource2="images/"+randomDiceImages2;
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
